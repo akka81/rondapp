fa0a831625e5acab39a16df14f120ace3ef2d8c4
akka81 <akka81@gmail.com>
2016-07-27 20:03:45 +0200

    geolocalization plugin added

---
+++ RondApp/RondApp/RondApp/Views/MapPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+using Plugin.Geolocator.Abstractions;
+
+namespace RondApp.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+
+            var p = GetCurrentPosition();
+           
+
+        }
+
+        public async Task<Position> GetCurrentPosition()
+        {
+            return await Plugin.Geolocator.CrossGeolocator.Current.GetPositionAsync(10000);
+         
+        }
+    
+    }
+}
