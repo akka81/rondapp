7d50b16d995eab4ea10068738469f628a1c62a0c
Marco Acchini <marco.acchini@reti.it>
2017-01-09 16:20:11 +0100

    IOS: dialer fix

--- RondApp/RondApp/RondApp.iOS/Services/Dialer.cs
+++ RondApp/RondApp/RondApp.iOS/Services/Dialer.cs
@@ -1,22 +1,35 @@
 ﻿using Foundation;
 using RondApp.Services;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using UIKit;
+using Xamarin.Forms;
 
+[assembly: Dependency(typeof(RondApp.iOS.Services.PhoneDialer))]
 namespace RondApp.iOS.Services
 {
-
 
     public class PhoneDialer : IDialer
     {
 
         public bool Dial(string number)
         {
-        
-            return UIApplication.SharedApplication.OpenUrl(
-                new NSUrl("tel:" + number));
+            //Makes a new NSUrl
+            var callURL = new NSUrl(string.Format("tel:{0}",number.Replace(" ","")));
+
+
+            if (UIApplication.SharedApplication.CanOpenUrl(callURL))
+            {
+                //After checking if phone can open NSUrl, it either opens the URL or outputs to the console.
+                return UIApplication.SharedApplication.OpenUrl(callURL);
+            }
+            else
+            {
+                //OUTPUT to console
+                Console.WriteLine("Can't make call");
+                return false;
+            }
+
+         
         }
     }
 }
