b64156c68aa26f384b2974e563287af2d87f0698
biancan1 <biancan1@CWDQ6YT32>
2017-01-18 10:16:18 +0100

    Modified font and nuget dependencies

--- RondApp/RondApp/RondApp/App.xaml.cs
+++ RondApp/RondApp/RondApp/App.xaml.cs
@@ -8,7 +8,28 @@ namespace RondApp
         public App()
         {
             InitializeComponent();
-           DbCenters DbMng = new DbCenters();
+
+            Application.Current.Resources["LargeFontSize"] = Device.OnPlatform(
+                iOS: Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                Android: Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                WinPhone: 20);
+
+            Application.Current.Resources["MediumFontSize"] = Device.OnPlatform(
+                iOS: Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Android: Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                WinPhone: 18);
+
+            Application.Current.Resources["SmallFontSize"] = Device.OnPlatform(
+                iOS: Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                Android: Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                WinPhone: 16);
+
+            Application.Current.Resources["XSmallFontSize"] = Device.OnPlatform(
+                iOS: Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                Android: Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                WinPhone: 14);
+
+            DbCenters DbMng = new DbCenters();
             DbMng.LoadDbData();
             MainPage = new Views.MainPage();
         }
